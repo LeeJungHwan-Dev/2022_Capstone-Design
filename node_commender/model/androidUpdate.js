6baea7f568a816395218f3019dfc8b2b7464bd77
JungHwan_Lee <93726941+LeeJungHwan-Dev@users.noreply.github.com>
2022-12-19 06:47:52 +0900

    Update Android Update Model_1

--- node_commender/model/androidUpdate.js
+++ node_commender/model/androidUpdate.js
@@ -23,11 +23,10 @@ async function updateAndroid(){
 
         result1.stdout.on('data',(result1)=>{
             console.log('다운로드 완료.');
+            console.log('앱 업데이트 검사');
+            shell.exec('adb connect 172.30.1.87:5555');
+            shell.exec('adb install -r ./downloads/' + item);
             })
-
-        console.log('앱 업데이트 검사');
-        shell.exec('adb connect 172.30.1.87:5555');
-        shell.exec('adb install -r /node_commender/downloads/' + item);
 
 
         /*result2.stdout.on('data',(result1)=>{
