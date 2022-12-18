c684c68fa146d5b8f52023dd83be127bc8476f93
JungHwan_Lee <93726941+LeeJungHwan-Dev@users.noreply.github.com>
2022-12-19 05:21:15 +0900

    Update Process Control model_1

--- node_commender/model/processControl.js
+++ node_commender/model/processControl.js
@@ -25,11 +25,13 @@ async function start_process (){
                 shell.exec('sudo chmod 770'+' downloads/'+item.process_name);
                 shell.exec('echo No InterNet 적용 & 실행 완료.');
                 shell.exec("sudo -u no-internet " + '"./downloads/server"' +" &");
+                return 0;
             }else if(item.process_status === "Allow"){
                 shell.exec('sudo chmod 777'+' downloads/'+item.process_name);
                 shell.exec('sudo chown raspi:raspi downloads/'+item.process_name);
                 shell.exec('echo Allow 적용 완료!');
                 shell.exec('sudo ./downloads/'+item.process_name +" &");
+                return 0;
             }
         
         }
